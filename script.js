05636a92afd727143f4f05853bf4f8c386808e85
Schornyyy <schornstein.lukas2001@outlook.de>
2023-12-08 13:27:14 +0100

    added contacts imports in css

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ async function setItem(key, value) {
     .then(res => res.json());
 }
 
+
 async function getItem(key) {
     const url = `${STORAGE_URL}?key=${key}&token=${STORAGE_TOKEN}`;
     return fetch(url).then(res => res.json());
