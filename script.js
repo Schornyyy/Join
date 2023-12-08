52dc97194c58b413f120fd7360ad5b82f65afd58
Schornyyy <schornstein.lukas2001@outlook.de>
2023-12-08 13:31:51 +0100

    added imports from contacts

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 const STORAGE_TOKEN = '1WGJFXCVU20M8HTON9ZTKKZ6YH67E347XBHJGJS3';
 const STORAGE_URL = 'https://remote-storage.developerakademie.org/item';
 
-
+/**
+ * 
+ * Speichert einen Value zum Key im Backend
+ * 
+ * @param {*} key 
+ * @param {*} value 
+ * 
+ */
 async function setItem(key, value) {
     const payload = { key, value, token: STORAGE_TOKEN };
     return fetch(STORAGE_URL, { method: 'POST', body: JSON.stringify(payload)})
     .then(res => res.json());
 }
 
+/**
+ * Nimmt die Daten aus dem Backend mit dem Key
+ * 
+ * @param {*} key 
+ * @returns 
+ */
 
 async function getItem(key) {
     const url = `${STORAGE_URL}?key=${key}&token=${STORAGE_TOKEN}`;
