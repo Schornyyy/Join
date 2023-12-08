c6961c52013390cc82126f47b4580e4be48c8029
Schornyyy <schornstein.lukas2001@outlook.de>
2023-12-08 13:19:01 +0100

    added setItem and getItem from Backend

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-"use strict";
+const STORAGE_TOKEN = 'HIER DEIN TOKEN EINFÜGEN';
+const STORAGE_URL = 'https://remote-storage.developerakademie.org/item';
+
+
+async function setItem(key, value) {
+    const payload = { key, value, token: STORAGE_TOKEN };
+    return fetch(STORAGE_URL, { method: 'POST', body: JSON.stringify(payload)})
+    .then(res => res.json());
+}
+
+async function getItem(key) {
+    const url = `${STORAGE_URL}?key=${key}&token=${STORAGE_TOKEN}`;
+    return fetch(url).then(res => res.json());
+}
