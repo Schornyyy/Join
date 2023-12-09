99174646f9acddb78166f373b01335fcfbab0853
Schornyyy <schornstein.lukas2001@outlook.de>
2023-12-09 13:05:13 +0100

    added tasks class

---
+++ assets/models/task.class.js
@@ -0,0 +1,46 @@
+class Task{
+
+    title;
+    description;
+    prio;
+    dueDate;
+    category;
+    assignedTo;
+    subtasks = [];
+    id;
+
+    constructor(title, dueDate, category, id) {
+        this.title = title;
+        this.dueDate = dueDate;
+        this.category = category;
+        this.id = id;
+    }
+
+    setDescription(description) {
+        this.description = description;
+    }
+
+    setPrio(prio) {
+        this.prio = prio;
+    }
+
+    setAsignedTo(contactName) {
+        this.assignedTo = contactName;
+    }
+
+    addSubtask(subtask) {
+        this.subtasks.push(subtask);
+    }
+
+    removeSubtask(index) {
+        this.subtasks.splice(index, 1)
+    }
+
+    async saveTask() {
+        let tasks = await getItem("tasks");
+        tasks.push(this);
+        setItem("tasks", tasks);
+    }
+    
+
+}
