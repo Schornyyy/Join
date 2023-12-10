f7a418d4bae0f007dd2948299e0884aa6fa8c47b
Schornyyy <schornstein.lukas2001@outlook.de>
2023-12-10 21:21:29 +0100

    updated task_form JS SelectPrio

--- assets/templates/add_tasks/add_tasks.js
+++ assets/templates/add_tasks/add_tasks.js
@@ -1,28 +1,32 @@
 let selectedPrio;
 
 function clickedPrio(prioElement) {
-    selectedPrio = prioElement.getAttribute("for");
-    let parentEle = prioElement.parentElement;
+    selectedPrio = prioElement;
     let prios = document.querySelectorAll("[data-prio]");
     prios.forEach((p) => {
+        let parentEle = p.parentElement;
         let prioAttribute = p.getAttribute("for");
-        if(prioAttribute.match(selectedPrio)) {
+        if(prioAttribute === selectedPrio) {
             switch (selectedPrio) {
                 case "prio-urgent":
                     parentEle.style = "background-color: #ff3d00;";
+                    p.style = "filter: brightness(0) invert(1);"
                     break;
                 case "prio-medium":
                     parentEle.style = "background-color: #ffa800;";
+                    p.style = "filter: brightness(0) invert(1);"
                     break;
                 case "prio-low":
                     parentEle.style = "background-color: #1fd7c1;";
+                    p.style = "filter: brightness(0) invert(1);"
                     break;
                 default:
                     break;
             }
+            console.log(prioAttribute);
         } else {
-            parentEle.style = "background-color: white;"
-            parentEle.style = "filter: none";
+            parentEle.style = "background-color: white;";
+            p.style = "filter: none;";
         }
     })
 }
