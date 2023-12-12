a9309cdf7a238cfb7ae8f6bb5802fbcce9ea1f8b
MartinHoffmann85 <138533657+MartinHoffmann85@users.noreply.github.com>
2023-12-12 16:53:57 +0100

    Contacts designed and logic

--- assets/templates/contacs/contacts.js
+++ assets/templates/contacs/contacts.js
@@ -1,6 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+    contactsInit();
+});
+
 async function contactsInit() {
     try {
+        console.log("Vor dem Kontakt Laden");
         contactsData = await fetchContactsData();
+        console.log("Nach dem Kontakt Laden");
         renderContacts();
     } catch (error) {
         console.error("Fehler beim Initialisieren der Kontakte:", error);
@@ -11,19 +17,49 @@ async function fetchContactsData() {
     try {
         const response = await fetch("../assets/templates/contacs/allContacts.json");
         const data = await response.json();
-        return data;
+        // Sortiere die Kontakte nach dem Namen
+        return data.sort((a, b) => a.contactName.localeCompare(b.contactName));
     } catch (error) {
         console.error("Fehler beim Laden der Kontakte:", error);
-        throw error; // Hier den Fehler erneut werfen, um ihn an contactsInit weiterzugeben
+        throw error;
     }
 }
 
 function renderContacts() {
-    const content = document.getElementById("contactsContentID");
+    const content = document.getElementById("contactsContent");
+
+    // Erstelle ein Objekt, um die Kontakte nach dem Anfangsbuchstaben zu gruppieren
+    const contactsByFirstLetter = {};
 
     contactsData.forEach(oneContact => {
-        const contactCard = document.createElement("div");
-        contactCard.classList.add("contact-card");
+        const firstLetter = oneContact.contactName.charAt(0).toUpperCase();
+
+        if (!contactsByFirstLetter[firstLetter]) {
+            // Erstelle einen neuen Abschnitt für den Buchstaben, wenn er noch nicht existiert
+            contactsByFirstLetter[firstLetter] = document.createElement("div");
+            contactsByFirstLetter[firstLetter].classList.add("oneContactContainer");
+
+            // Erstelle die Buchstaben-Spalte
+            const letterContainer = document.createElement("div");
+            letterContainer.classList.add("letter-column");
+
+            // Erstelle den Buchstaben
+            const letterHeader = document.createElement("h2");
+            letterHeader.textContent = firstLetter;
+            letterHeader.classList.add("contact-first-letter");
+            letterContainer.appendChild(letterHeader);
+
+            contactsByFirstLetter[firstLetter].appendChild(letterContainer);
+        }
+
+        // Erstelle das img-Element für das Kontaktbild
+        const contactImgElement = document.createElement("img");
+        contactImgElement.src = oneContact.contactImg;
+        contactImgElement.classList.add("contactImg");
+
+        // Erstelle einen Container für den Namen und die E-Mail-Adresse
+        const infoContainer = document.createElement("div");
+        infoContainer.classList.add("contact-info-container");
 
         const nameElement = document.createElement("h2");
         nameElement.textContent = oneContact.contactName;
@@ -31,9 +67,19 @@ function renderContacts() {
         const contactMailAddressElement = document.createElement("a");
         contactMailAddressElement.textContent = `${oneContact.contactMailAdress}`;
 
-        contactCard.appendChild(nameElement);
-        contactCard.appendChild(contactMailAddressElement);
+        // Füge die Elemente zum Info-Container hinzu
+        infoContainer.appendChild(nameElement);
+        infoContainer.appendChild(contactMailAddressElement);
 
-        content.appendChild(contactCard);
+        // Füge das Bild zur linken Spalte hinzu
+        contactsByFirstLetter[firstLetter].appendChild(contactImgElement);
+
+        // Füge den Info-Container zur rechten Spalte hinzu
+        contactsByFirstLetter[firstLetter].appendChild(infoContainer);
+    });
+
+    // Füge die Abschnitte in den HTML-Code ein
+    Object.values(contactsByFirstLetter).forEach(section => {
+        content.appendChild(section);
     });
 }
