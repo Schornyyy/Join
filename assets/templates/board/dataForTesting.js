af260ec21224e008ca9f6a1959512cc11e3a140b
MetalRick84 <lernen@reinitestet.at>
2023-12-12 12:58:58 +0100

    board: add more tasks for testing

--- assets/templates/board/dataForTesting.js
+++ assets/templates/board/dataForTesting.js
@@ -1,14 +1,16 @@
 let tasks= [
     {
+        id: 0,
         title: 'eins',
         description: 'erste Aufgabe zum testen von Board',
         prio: 'urgent', //string oder number?
         category: 'testing Board', //Farbe für Kategorien muss wo gespeichert werden
         assignedTo: [], //eigene Klasse für Kontake bzw. User?
         subtasks: [],
-        status: 'in progress' //brauchen wir zum Einordnen in eine section (bzw. spalte)
+        status: 'in progress' //brauchen wir zum Einordnen in eine section (bzw. spalte). Vielleicht statt finished?
     },
     {
+        id: 1,
         title: 'zwo',
         description: 'zweite Aufgabe kommt in Spalte await feedback',
         prio: 'medium',
@@ -18,6 +20,7 @@ let tasks= [
         status: 'await feedback'
     },
     {
+        id: 2,
         title: 'drei',
         description: 'lorem ipsum',
         prio: 'medium',
@@ -25,5 +28,38 @@ let tasks= [
         assignedTo: [],
         subtasks: [],
         status: 'done'
+    },
+    {
+        id: 3,
+        title: 'vier',
+        description: 'eine zweite Aufgabe für die Spalte in progress',
+        prio: 'low',
+        category: 'irgendwas',
+        assignedTo: [],
+        subtasks: [],
+        status: 'done'
+    },
+    {
+        id: 4,
+        title: 'fünf',
+        description: 'Ich habe sogar Unteraufgaben ;)',
+        prio: 'low',
+        category: 'irgendwas',
+        assignedTo: [],
+        subtasks: [0, 1],
+        status: 'done'
+    },
+];
+
+let subtasks= [
+    {
+        id: 0,
+        title: 'erste Unteraufgabe',
+        finished: false
+    },
+    {
+        id: 1,
+        title: 'zweite Unteraufgabe',
+        finished: true
     }
 ];
