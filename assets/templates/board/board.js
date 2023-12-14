5bded7efd117401040e5fc880b07bc27c22951a6
MetalRick <lernen@reinitestet.at>
2023-12-14 08:15:09 +0100

    board: subtasks getter

--- assets/templates/board/board.js
+++ assets/templates/board/board.js
@@ -2,10 +2,98 @@
 
 let urlPrefix= './assets';
 
+let tasksForTesting= [
+    {
+        id: 0,
+        title: 'eins',
+        description: 'erste Aufgabe zum testen von Board',
+        prio: 'urgent', //string oder number?
+        category: 'testing Board', //Farbe für Kategorien muss wo gespeichert werden
+        assignedTo: [], //eigene Klasse für Kontake bzw. User?
+        subtasks: [],
+        status: 'in progress' //brauchen wir zum Einordnen in eine section (bzw. spalte). Vielleicht statt finished?
+    },
+    {
+        id: 1,
+        title: 'zwo',
+        description: 'zweite Aufgabe kommt in Spalte await feedback',
+        prio: 'medium',
+        category: 'another category', //
+        assignedTo: [],
+        subtasks: [],
+        status: 'await feedback'
+    },
+    {
+        id: 2,
+        title: 'drei',
+        description: 'lorem ipsum',
+        prio: 'medium',
+        category: 'third category',
+        assignedTo: [],
+        subtasks: [],
+        status: 'done'
+    },
+    {
+        id: 3,
+        title: 'vier',
+        description: 'eine zweite Aufgabe für die Spalte in progress',
+        prio: 'low',
+        category: 'irgendwas',
+        assignedTo: [],
+        subtasks: [],
+        status: 'in progress'
+    },
+    {
+        id: 4,
+        title: 'fünf',
+        description: 'Ich habe sogar Unteraufgaben ;)',
+        prio: 'low',
+        category: 'irgendwas',
+        assignedTo: [],
+        subtasks: [0, 1],
+        status: 'done'
+    },
+    {
+        id: 5,
+        title: 'sechs',
+        description: 'Die Spalte ToDo braucht auch eine Aufgabe.',
+        prio: 'low',
+        category: 'farbe',
+        assignedTo: [],
+        subtasks: [],
+        status: 'todo'
+    },
+];
+
+let subtasksForTesting= [
+    {
+        id: 0,
+        title: 'erste Unteraufgabe',
+        finished: false
+    },
+    {
+        id: 1,
+        title: 'zweite Unteraufgabe',
+        finished: true
+    }
+];
+
+function boardInitForTesting() {
+    let contentElement= document.getElementById('content');
+    contentElement.classList.remove('d-none');
+}
+
+function testen() {
+    let tempTask= tasksForTesting[4];
+    console.log(getAmountOfSubtasks(tempTask));
+    console.log(getAmountOfFinishedSubtasks(tempTask));
+    console.log(getSubtaskById(2));
+}
 
 //////////////// INIT
 
 function boardInit() {
+    boardInitForTesting();
     renderBoard();
 }
 
@@ -20,15 +108,13 @@ function renderBoard() {
 
 function renderTasks(containerID, status) {
     let cardContainer= document.getElementById(containerID);
-    console.log('renderTasks(), containerID: ' + containerID);
-    console.log('renderTasks(), cardContainer: ' + cardContainer);
-    let tasksStatus= getTasksFromStatus(tasks, status);
+    let tasksStatus= getTasksFromStatus(tasksForTesting, status);
     if (tasksStatus.length > 0) {
         cardContainer.innerHTML= tasksToHML(tasksStatus);
     }
 }
 
-//////////////// GET TASKS
+//////////////// GETTER
 
 function getTasksFromStatus(tasksArray, status){
     return tasksArray.filter((task) => task.status==status);
@@ -41,6 +127,30 @@ function tasksToHML(tasksParam) {
     }
     return output;
 }   
+
+function getAmountOfSubtasks(task) {
+    return task.subtasks.length;
+}
+
+function getAmountOfFinishedSubtasks(task) {
+    return getFinishedSubtasks(task).length;
+}
+
+function getFinishedSubtasks(task) {
+    output= [];
+    let subtaskTemp;
+    task.subtasks.forEach(subtaskID => {
+        subtaskTemp= getSubtaskById(subtaskID);
+        if (subtaskTemp.finished) {
+            output.push(subtaskTemp);
+        }
+    });
+    return output;
+}
+
+function getSubtaskById(idParam) {
+    return subtasksForTesting.find(subtask => subtask.id==idParam);
+}
 
 //////////////// TO HTML
 
