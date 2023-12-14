2fdd4d8484b5dbd43a78addb8916fea2e9101551
Schornyyy <schornstein.lukas2001@outlook.de>
2023-12-14 10:53:44 +0100

    added users

--- assets/js/register.js
+++ assets/js/register.js
@@ -3,3 +3,14 @@ function registerUser(name, email, password) {
     users.push(user);
     saveUsers();
 }
+
+function emailAlreadyTaken(email) {
+    let emails = users.find(a => a.email === email);
+    let c = false;
+
+    if(emails != null || emails != undefined) {
+        c = true;
+    }
+
+    return c;
+}
