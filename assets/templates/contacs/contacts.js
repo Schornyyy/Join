9b3252dda2a91c06a3dd331ce9fbea64a1466a54
MartinHoffmann85 <138533657+MartinHoffmann85@users.noreply.github.com>
2023-12-15 02:11:23 +0100

    Add contact button and screen added

--- assets/templates/contacs/contacts.js
+++ assets/templates/contacs/contacts.js
@@ -6,11 +6,27 @@ async function contactsInit() {
     try {
         console.log("Vor dem Kontakt Laden");
         contactsData = await fetchContactsData();
-        console.log("Nach dem Kontakt Laden");
+        console.log("Nach dem Kontakt Laden");        
         renderContacts();
+        renderAddContactButton();
     } catch (error) {
         console.error("Fehler beim Initialisieren der Kontakte:", error);
     }
+}
+
+function renderAddContactButton() {
+    const content = document.getElementById("contactsContent");
+    const addContactButtonContainer = document.createElement("div");
+    addContactButtonContainer.classList.add("addContactButtonContainer");    
+    addContactButtonContainer.innerHTML = '<img src="../assets/img/addContactButtonMobile.svg" class="addContactImage" onclick="handleAddContactClick()">';  // onclick-Funktion direkt im HTML-Code
+    content.appendChild(addContactButtonContainer);
+}
+
+// Diese Funktion wird direkt im HTML-Code aufgerufen
+function handleAddContactClick() {
+    addContactScreen();
+    console.log("Bild wurde angeklickt!");
+    
 }
 
 async function fetchContactsData() {
@@ -82,4 +98,29 @@ function renderContacts() {
     Object.values(contactsByFirstLetter).forEach(section => {
         content.appendChild(section);
     });
+}
+
+function addContactScreen() {
+    const content = document.getElementById("contactsContent");
+    content.innerHTML = `<div class="addContactContainerHeader">
+                            <h1 class="addContactH1">Add contact</h1>
+                            <p class="addContactText">Tasks are better with a team!</p>
+                        </div>
+
+                        <div>
+                        <img src="../assets/img/addContactBlankUserImg.svg" alt="">
+                        </div>
+
+                        <div class="addContactContainerFooter">
+                            <input class="addContactInputName" type="text" required placeholder="Name">
+                            <input class="addContactInputMailAddresss" type="text" required placeholder="E Mail">
+                            <input class="addContactInputPhone" type="text" required placeholder="Phone">
+
+                        <div>
+                            <img class="createContactButtonImg" src="../assets/img/createContactButton.svg" alt="">
+                        </div>
+                        </div>
+                        `;
+
+        
 }
