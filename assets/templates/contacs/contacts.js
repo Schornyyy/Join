5f537369f15542cb3ebe31b05208b11e739085f7
MartinHoffmann85 <138533657+MartinHoffmann85@users.noreply.github.com>
2023-12-19 00:20:53 +0100

    saveContactsData function added with error

--- assets/templates/contacs/contacts.js
+++ assets/templates/contacs/contacts.js
@@ -22,19 +22,15 @@ function renderAddContactButton() {
     content.appendChild(addContactButtonContainer);
 }
 
-// Diese Funktion wird direkt im HTML-Code aufgerufen
-function handleAddContactClick() {
+function handleAddContactClick() {  // Diese Funktion wird direkt im HTML-Code aufgerufen
     addContactScreen();
-    console.log("Bild wurde angeklickt!");
-    
 }
 
 async function fetchContactsData() {
     try {
         const response = await fetch("../assets/templates/contacs/allContacts.json");
-        const data = await response.json();
-        // Sortiere die Kontakte nach dem Namen
-        return data.sort((a, b) => a.contactName.localeCompare(b.contactName));
+        const data = await response.json();        
+        return data.sort((a, b) => a.contactName.localeCompare(b.contactName));  // Sortiert die Kontakte nach dem Namen
     } catch (error) {
         console.error("Fehler beim Laden der Kontakte:", error);
         throw error;
@@ -42,60 +38,42 @@ async function fetchContactsData() {
 }
 
 function renderContacts() {
-    const content = document.getElementById("contactsContent");
-
-    // Erstelle ein Objekt, um die Kontakte nach dem Anfangsbuchstaben zu gruppieren
-    const contactsByFirstLetter = {};
+    const content = document.getElementById("contactsContent");    
+    const contactsByFirstLetter = {};  // Erstellt ein Objekt, um die Kontakte nach dem Anfangsbuchstaben zu gruppieren
 
     contactsData.forEach(oneContact => {
         const firstLetter = oneContact.contactName.charAt(0).toUpperCase();
-
-        if (!contactsByFirstLetter[firstLetter]) {
-            // Erstelle einen neuen Abschnitt für den Buchstaben, wenn er noch nicht existiert
-            contactsByFirstLetter[firstLetter] = document.createElement("div");
-            contactsByFirstLetter[firstLetter].classList.add("oneContactContainer");
-
-            // Erstelle die Buchstaben-Spalte
-            const letterContainer = document.createElement("div");
-            letterContainer.classList.add("letter-column");
-
-            // Erstelle den Buchstaben
-            const letterHeader = document.createElement("h2");
+        if (!contactsByFirstLetter[firstLetter]) {            
+            contactsByFirstLetter[firstLetter] = document.createElement("div");  // Erstelle einen neuen Abschnitt für den Buchstaben, wenn er noch nicht existiert
+            contactsByFirstLetter[firstLetter].classList.add("letterAndContactsContainer");            
+            const letterContainer = document.createElement("div");  // Erstelle die Buchstaben-Spalte
+            letterContainer.classList.add("letter-column");           
+            const letterHeader = document.createElement("h2");   // Erstelle den Buchstaben
             letterHeader.textContent = firstLetter;
             letterHeader.classList.add("contact-first-letter");
             letterContainer.appendChild(letterHeader);
-
             contactsByFirstLetter[firstLetter].appendChild(letterContainer);
         }
-
-        // Erstelle das img-Element für das Kontaktbild
-        const contactImgElement = document.createElement("img");
+        const oneContactContainer = document.createElement("div");  // Erstellt einen Container für einen Kontakt mit dem Namen und der E-Mail-Adresse
+        const userImgContainer = document.createElement("div");  // Erstellt einen Container für einen Kontakt mit dem Bild
+        const contactImgElement = document.createElement("img");  // Erstellt das img-Element für das Kontaktbild        
         contactImgElement.src = oneContact.contactImg;
-        contactImgElement.classList.add("contactImg");
-
-        // Erstelle einen Container für den Namen und die E-Mail-Adresse
-        const infoContainer = document.createElement("div");
+        contactImgElement.classList.add("contactImg");        
+        const infoContainer = document.createElement("div");  // Erstellt einen Container für den Namen und die E-Mail-Adresse
         infoContainer.classList.add("contact-info-container");
-
         const nameElement = document.createElement("h2");
         nameElement.textContent = oneContact.contactName;
-
         const contactMailAddressElement = document.createElement("a");
         contactMailAddressElement.textContent = `${oneContact.contactMailAdress}`;
-
-        // Füge die Elemente zum Info-Container hinzu
-        infoContainer.appendChild(nameElement);
-        infoContainer.appendChild(contactMailAddressElement);
-
-        // Füge das Bild zur linken Spalte hinzu
-        contactsByFirstLetter[firstLetter].appendChild(contactImgElement);
-
-        // Füge den Info-Container zur rechten Spalte hinzu
-        contactsByFirstLetter[firstLetter].appendChild(infoContainer);
-    });
-
-    // Füge die Abschnitte in den HTML-Code ein
-    Object.values(contactsByFirstLetter).forEach(section => {
+        userImgContainer.appendChild(contactImgElement);  // Fügt das contactImgElement dem infoContainer hinzu
+        oneContactContainer.appendChild(userImgContainer);        
+        infoContainer.appendChild(nameElement);  // Füge die Elemente zum Info-Container hinzu
+        infoContainer.appendChild(contactMailAddressElement);  
+        oneContactContainer.appendChild(infoContainer);        
+        contactsByFirstLetter[firstLetter].appendChild(contactImgElement);   // Fügt das Bild zur linken Spalte hinzu        
+        contactsByFirstLetter[firstLetter].appendChild(infoContainer);  // Fügt den Info-Container zur rechten Spalte hinzu
+    });    
+    Object.values(contactsByFirstLetter).forEach(section => {  // Füge die Abschnitte in den HTML-Code ein // Diese Zeile Verursacht einen Fehler bisher !
         content.appendChild(section);
     });
 }
@@ -116,8 +94,78 @@ function addContactScreen() {
                                 <input class="addContactInputName" type="text" required placeholder="Name">
                                 <input class="addContactInputMailAddresss" type="text" required placeholder="E Mail">
                                 <input class="addContactInputPhone" type="text" required placeholder="Phone">
-                                <img class="createContactButtonImg" src="../assets/img/createContactButton.svg" alt="">
+                                <img class="createContactButtonImg" src="../assets/img/createContactButton.svg" alt="" onclick="createContact()">
                             <div>
                         </form>
-                        `;  
+                        `;
+    hideHeaderAndFooter();
+}
+
+function hideHeaderAndFooter() {    
+    const mobileHeader = document.querySelector(".mobileHeader");  // Verstecke mobileHeader und menuTemplate
+    const menuTemplate = document.querySelector(".menuTemplate");
+        mobileHeader.style.display = "none";
+        menuTemplate.style.display = "none";
+}
+
+
+
+
+
+
+
+
+function createContact() {
+    const nameInput = document.querySelector('.addContactInputName');
+    const mailInput = document.querySelector('.addContactInputMailAddresss');
+    const phoneInput = document.querySelector('.addContactInputPhone');
+
+    // Daten aus den Input-Feldern lesen
+    const newName = nameInput.value.trim();
+    const newMail = mailInput.value.trim();
+    const newPhone = phoneInput.value.trim();
+
+    // Überprüfen, ob alle Felder ausgefüllt sind
+    if (newName === '' || newMail === '' || newPhone === '') {
+        alert('Bitte füllen Sie alle Felder aus.');
+        return;
+    }
+
+    // Neuen Kontakt erstellen
+    const newContact = {
+        contactName: newName,
+        contactMailAdress: newMail,
+        contactPhone: newPhone,        
+    };
+
+    // Daten zum JSON-Array hinzufügen
+    contactsData.push(newContact);
+
+    // JSON-Array speichern (z.B. auf dem Server)
+    saveContactsData(contactsData);
+
+    // Zurück zur Kontaktliste wechseln
+    renderContacts();
+}
+
+async function saveContactsData(data) {
+    try {
+        const authToken = STORAGE_TOKEN; // Authentifizierungstoken einfügen
+        const response = await fetch('../assets/templates/contacs/allContacts.json', {
+            method: 'Post', // Änderet die Methode auf PUT oder POST, je nach Serverkonfiguration
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${authToken}`, // Füget den Authentifizierungstoken zum Header hinzu
+            },
+            body: JSON.stringify(data),
+        });
+
+        if (!response.ok) {
+            throw new Error('Fehler beim Speichern der Kontakte. Status: ' + response.status);
+        }
+
+        console.log('Kontakte erfolgreich gespeichert.');
+    } catch (error) {
+        console.error('Fehler beim Speichern der Kontakte:', error);        
+    }
 }
