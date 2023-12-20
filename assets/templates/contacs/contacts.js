4d5e46d5a522adeba8893b2b7232af78f354e6f1
MartinHoffmann85 <138533657+MartinHoffmann85@users.noreply.github.com>
2023-12-20 01:34:38 +0100

    contacts new design layout

--- assets/templates/contacs/contacts.js
+++ assets/templates/contacs/contacts.js
@@ -2,18 +2,20 @@ document.addEventListener("DOMContentLoaded", function () {
     contactsInit();
 });
 
-async function contactsInit() {
-    const content = document.getElementById("contactsContent");
-    content.innerHTML = "";    
+window.addEventListener("load", function () {
+    contactsInit();
+});
+
+async function contactsInit() {        
     try {
         console.log("Vor dem Kontakt Laden");
         contactsData = await fetchContactsData();
-        console.log("Nach dem Kontakt Laden");        
+        console.log("Nach dem Kontakt Laden");               
         renderContacts();
         renderAddContactButton();
     } catch (error) {
         console.error("Fehler beim Initialisieren der Kontakte:", error);
-    }
+    }    
     showHeaderAndFooter();
 }
 
@@ -42,43 +44,40 @@ async function fetchContactsData() {
 
 function renderContacts() {
     const content = document.getElementById("contactsContent");
-       
-    const contactsByFirstLetter = {};  // Erstellt ein Objekt, um die Kontakte nach dem Anfangsbuchstaben zu gruppieren    
+    content.innerHTML = "";
+
+    const contactsByFirstLetter = {};
 
     contactsData.forEach(oneContact => {
         const firstLetter = oneContact.contactName.charAt(0).toUpperCase();
-        if (!contactsByFirstLetter[firstLetter]) {            
-            contactsByFirstLetter[firstLetter] = document.createElement("div");  // Erstelle einen neuen Abschnitt für den Buchstaben, wenn er noch nicht existiert
-            contactsByFirstLetter[firstLetter].classList.add("letterAndContactsContainer");            
-            const letterContainer = document.createElement("div");  // Erstelle die Buchstaben-Spalte
-            letterContainer.classList.add("letter-column");           
-            const letterHeader = document.createElement("h2");   // Erstelle den Buchstaben
-            letterHeader.textContent = firstLetter;
-            letterHeader.classList.add("contact-first-letter");
-            letterContainer.appendChild(letterHeader);
-            contactsByFirstLetter[firstLetter].appendChild(letterContainer);
+
+        if (!contactsByFirstLetter[firstLetter]) {
+            contactsByFirstLetter[firstLetter] = /*html*/`
+                <div class="letterAndContactsContainer">
+                    <div class="letter-column">
+                        <h2 class="contact-first-letter">${firstLetter}</h2>
+                    </div>
+                </div>
+            `;
         }
-        const oneContactContainer = document.createElement("div");  // Erstellt einen Container für einen Kontakt mit dem Namen und der E-Mail-Adresse
-        const userImgContainer = document.createElement("div");  // Erstellt einen Container für einen Kontakt mit dem Bild
-        const contactImgElement = document.createElement("img");  // Erstellt das img-Element für das Kontaktbild        
-        contactImgElement.src = oneContact.contactImg;
-        contactImgElement.classList.add("contactImg");        
-        const infoContainer = document.createElement("div");  // Erstellt einen Container für den Namen und die E-Mail-Adresse
-        infoContainer.classList.add("contact-info-container");
-        const nameElement = document.createElement("h2");
-        nameElement.textContent = oneContact.contactName;
-        const contactMailAddressElement = document.createElement("a");
-        contactMailAddressElement.textContent = `${oneContact.contactMailAdress}`;
-        userImgContainer.appendChild(contactImgElement);  // Fügt das contactImgElement dem infoContainer hinzu
-        oneContactContainer.appendChild(userImgContainer);        
-        infoContainer.appendChild(nameElement);  // Füget die Elemente zum Info-Container hinzu
-        infoContainer.appendChild(contactMailAddressElement);  
-        oneContactContainer.appendChild(infoContainer);        
-        contactsByFirstLetter[firstLetter].appendChild(contactImgElement);   // Fügt das Bild zur linken Spalte hinzu        
-        contactsByFirstLetter[firstLetter].appendChild(infoContainer);  // Fügt den Info-Container zur rechten Spalte hinzu
-    });    
-    Object.values(contactsByFirstLetter).forEach(section => {  // Fügt die Abschnitte in den HTML-Code ein // Diese Zeile Verursacht einen Fehler bisher !
-        content.appendChild(section);
+
+        const oneContactContainer = /*html*/`
+            <div class="justifyContentCenter">
+                <div>
+                    <img src="${oneContact.contactImg}" class="contactImg">
+                </div>
+                <div class="contact-info-container">
+                    <h2>${oneContact.contactName}</h2>
+                    <a>${oneContact.contactMailAdress}</a>
+                </div>
+            </div>
+        `;
+
+        contactsByFirstLetter[firstLetter] += oneContactContainer;
+    });
+
+    Object.values(contactsByFirstLetter).forEach(section => {
+        content.innerHTML += section;
     });
 }
 
