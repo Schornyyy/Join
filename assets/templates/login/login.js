6c3fe51313e22d867eb013496fe3a9548b3c81f6
Konstantin <kav.pbg@gmail.com>
2023-12-30 15:23:48 -0300

    Log in style

--- assets/templates/login/login.js
+++ assets/templates/login/login.js
@@ -1,54 +1,54 @@
 let user;
 let rememberMe = false;
 let data = {
-    email: "",
-    rememberMe: rememberMe,
+  email: "",
+  rememberMe: rememberMe,
 };
-
 
 /**
  * Logged den User ein.
  */
 function login() {
-    let userExists = userEmailExists();
-    let userEmail = document.getElementById("login-input-email").value;
-    let password = document.getElementById("login-input-password").value;
-    let errorMsg = document.getElementById("login-error");
-    errorMsg.style = "color: red";
-    
-    if(!userExists) return;
-    user = findUserByEmail(userEmail);
+  let userExists = userEmailExists();
+  let userEmail = document.getElementById("login-input-email").value;
+  let password = document.getElementById("login-input-password").value;
+  let errorMsg = document.getElementById("login-error");
+  errorMsg.style = "color: red";
 
-    let matchPassword = userPasswordMatch(password);
-    if(!matchPassword) {
-        errorMsg.innerHTML = "Wrong password";
-        return;
-    }
-    data.email = userEmail;
-    localStorage.setItem("userData", JSON.stringify(data));
-    window.location.href = "/index.html";
+  if (!userExists) return;
+  user = findUserByEmail(userEmail);
+
+  let matchPassword = userPasswordMatch(password);
+  if (!matchPassword) {
+    errorMsg.innerHTML = "Wrong password";
+    return;
+  }
+  data.email = userEmail;
+  localStorage.setItem("userData", JSON.stringify(data));
+  window.location.href = "/index.html";
 }
-
 
 /**
  * Handled die RememberMe Function
- * @param {Checkbox} element 
+ * @param {Checkbox} element
  */
 function handleRememberme(element) {
-    let ele = element;
+  let ele = element;
 
-    rememberMe ? ele.setAttribute("checked", false) : ele.setAttribute("checked", true) ;
+  rememberMe
+    ? ele.setAttribute("checked", false)
+    : ele.setAttribute("checked", true);
 
-    rememberMe = !rememberMe;
+  rememberMe = !rememberMe;
 }
 
 /**
  * Checkt ob das Password übereinstimmt.
- * @param {String} password 
+ * @param {String} password
  * @returns true | false
  */
 function userPasswordMatch(password) {
-    return user.password.match(password);
+  return user.password.match(password);
 }
 
 /**
@@ -56,40 +56,23 @@ function userPasswordMatch(password) {
  * @returns true | false
  */
 function userEmailExists() {
-    let errorMsg = document.getElementById("login-error");
-    errorMsg.style = "color: red";
-    let userEmail = document.getElementById("login-input-email").value;
-    let c = true;
+  let errorMsg = document.getElementById("login-error");
+  errorMsg.style = "color: red";
+  let userEmail = document.getElementById("login-input-email").value;
+  let c = true;
 
-    if(!findUserByEmail(userEmail)) {
-        errorMsg.innerHTML = "No User with Email: " + userEmail;
-        c = false;
-    }
-    return c;
+  if (!findUserByEmail(userEmail)) {
+    errorMsg.innerHTML = "No User with Email: " + userEmail;
+    c = false;
+  }
+  return c;
 }
 
-
 /**
- * 
- * @param {String} email 
- * @returns gibt den User als 
+ *
+ * @param {String} email
+ * @returns gibt den User als
  */
 function findUserByEmail(email) {
-    return users.find(a => a.email===email);
+  return users.find((a) => a.email === email);
 }
-
-/**
- * Die Input felder im focus sollen Border-color ein eigene Farbe haben.
- */
-function changeBorderColor() {
-    var container = document.querySelector('.emailDevision');
-    container.classList.add('active');
-  }
-  
-  /**
-   * Den Focus/ die Farbe wieder entfernen.
-   */
-  function resetBorderColor() {
-    var container = document.querySelector('.emailDevision');
-    container.classList.remove('active');
-  }
--- assets/worker/inputFocus/inputFocus.js
+++ assets/worker/inputFocus/inputFocus.js
@@ -1,28 +1,39 @@
+/**
+ * Die Input felder im focus sollen Border-color ein eigene Farbe haben.
+ */
 function changeBorderColor(containerId) {
-  var container = document.getElementById(containerId);
+  let container = document.getElementById(containerId);
   container.classList.add('active');
 }
 
+  /**
+   * Den Focus/ die Farbe wieder entfernen.
+   */
 function resetBorderColor(containerId) {
-  var container = document.getElementById(containerId);
+  let container = document.getElementById(containerId);
   container.classList.remove('active');
 }
 
+/**
+ * Input field - border red
+ * @param {*} containerId 
+ */
 function showError(containerId) {
-  var container = document.getElementById(containerId);
+  let container = document.getElementById(containerId);
   container.classList.add('error');
 }
 
+// ??? funktion unnötig ???
 // Beispiel für die Verwendung, wenn eine falsche E-Mail eingegeben wurde
-function validateEmail() {
-  var emailInput = document.getElementById('emailInput').value;
+// function validateEmail() {
+//   let emailInput = document.getElementById('emailInput').value;
   
   // Prüfen der E-Mail (z. B. eine einfache Überprüfung auf '@')
-  if (!emailInput.includes('@')) {
-    showError('emailContainer');
-  } else {
-    resetBorderColor('emailContainer');
-  }
-}
+//   if (!emailInput.includes('@')) {
+//     showError('emailContainer');
+//   } else {
+//     resetBorderColor('emailContainer');
+//   }
+// }
 
 // Weitere Funktionen für das Überprüfen von Passwort usw. könnten ähnlich erstellt werden
