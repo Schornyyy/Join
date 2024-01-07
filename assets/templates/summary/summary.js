80ccb4bae1259aefc634076bb2d66cc2e359b042
Konstantin Aksenov <kav.pbg@gmail.com>
2024-01-07 21:07:09 +0100

    sammary style

--- assets/templates/summary/summary.js
+++ assets/templates/summary/summary.js
@@ -1,7 +1,6 @@
 "use strict";
 
-
-// TODO 
+// TODO
 // greeting();
 
 // greetingMobile();
@@ -12,3 +11,38 @@
 //   if (loggedIn) activeSummary();
 // }
 
+/**
+ * Symmary content animate images.
+ * @param {Image to animate whit hover} element
+ */
+// function changeImage(element) {
+//   const img = element.querySelector('.summaryAnimateProgramm');
+// img.src = "assets/img/summary/summaryWhiteEdit.svg"
+// }
+
+// function changeImageBack(element) {
+//   const img = element.querySelector('.summaryAnimateProgramm');
+// img.src = "assets/img/summary/summaryGrayEdit.svg"
+// }
+
+/**
+ * Symmary content animate images.
+ * @param {Image to animate whit hover} element
+ */
+function changeImage(element) {
+  const img = element.querySelector(".summaryAnimateProgramm");
+  if (img.classList.contains("editImage")) {
+    img.src = "assets/img/summary/summaryWhiteEdit.svg";
+  } else if (img.classList.contains("checkImage")) {
+    img.src = "assets/img/summary/summaryCheckWhite.svg";
+  }
+}
+
+function changeImageBack(element) {
+  const img = element.querySelector(".summaryAnimateProgramm");
+  if (img.classList.contains("editImage")) {
+    img.src = "assets/img/summary/summaryGrayEdit.svg";
+  } else if (img.classList.contains("checkImage")) {
+    img.src = "assets/img/summary/summaryCheckGray.svg";
+  }
+}
