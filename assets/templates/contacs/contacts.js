1c45c9fdc840893e0df407992d0978d7dec02f0a
MartinHoffmann85 <138533657+MartinHoffmann85@users.noreply.github.com>
2024-01-09 20:54:02 +0100

    contacts.js clean code final

--- assets/templates/contacs/contacts.js
+++ assets/templates/contacs/contacts.js
@@ -1,41 +1,41 @@
+let nextContactId; // ID-counter for next contact-ID
+let lastClickedContactId; // Last clicked contact-ID
 
-let nextContactId; // ID-Zähler für die nächste Kontakt-ID
-let lastClickedContactId;
-
-async function contactsInit() {  
+// contacts inti function
+async function contactsInit() {  // Init function for contacts
   try {   
-    initializeContactId();
-    initializeView();
-    showHeaderAndFooter();
-    renderAddContactButton();   
-  } catch (error) {
+    initializeContactId();  // Get contact ID´s
+    initializeView();  // Check if mobile or desktop view for css media querry
+    showHeaderAndFooter();  // Show header and footer for mobile
+    renderAddContactButton();  // Create add contact button
+  } catch (error) {  // catch error
     console.error("Fehler beim Initialisieren der Kontakte:", error);
   }
 }
 
-
-function initializeContactId() {
+function initializeContactId() {  // Get contact ID´s
   nextContactId = currentUser.contacts.length;
 }
 
-function initializeView() {
+function initializeView() {  // Check if mobile or desktop view for css media querry
   const isMobile = window.innerWidth < 768;
   if (isMobile) {      
-    renderContacts();  
-    hidecontactsContentRightSideDesktop();
+    renderContacts();  // Render contacts for mobile view
+    hidecontactsContentRightSideDesktop();  // Hide contact right side container in mobile view
   } else {      
-    renderContactsDesktop();  
+    renderContactsDesktop();  // Render contacts for desktop view
     showContactsContentRightSideDesktop();
   }
   contactsContentBackgroundColorWhite();
 }
 
+//---------------------------------------------------------------------------------------
 function handleAddContactClick() {
-  addContactScreen(); // Diese Funktion wird direkt im HTML-Code aufgerufen
+  addContactScreen(); // This function is called in HTML-Code and shows the "add contact screen" to add a new contact person
 }
 
- // Developer tool (Nur für die entwickler, nicht für das Projekt ansich notwendig)
- async function deleteContactDataById() {  // Funktion deleteContactDataById ist für einen LokalStorage clear da falls ein Kontakt doppelt gespeichert wurde
+// Developer tool (only for developer, not needed for the project himself)
+async function deleteContactDataById() {  // Funktion deleteContactDataById ist für einen LokalStorage clear da falls ein Kontakt doppelt gespeichert wurde
   try {
     localStorage.clear();  // Lösche alle Daten im localStorage
     contactsData = await fetchContactsData();  // Lade die Daten vom Server
