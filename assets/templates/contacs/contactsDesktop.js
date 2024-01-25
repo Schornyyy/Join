0dc378cf241e044ce7fb13b00fdffa6813f261e3
MartinHoffmann85 <138533657+MartinHoffmann85@users.noreply.github.com>
2024-01-25 23:30:28 +0100

    contactsContentRightSide desktop view clear function added

--- assets/templates/contacs/contactsDesktop.js
+++ assets/templates/contacs/contactsDesktop.js
@@ -263,9 +263,9 @@ async function createContactDesktop() {
   let createdContact = new Contact(newName, newMail, newPhone, getRandomColorHex(), currentUser.name, currentUser.contacts.length + 1);
   await currentUser.contacts.push(createdContact);
   await currentUser.save();
-  hideOverlay();
+  hideOverlay();v  
+  setTimeout(() => {contactsInit();}, 1000 * 1.5);
   clearAddContactDesktopRightSideContainer();
-  contactsInit();
 }
 
 /**
