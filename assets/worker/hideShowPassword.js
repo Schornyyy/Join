d66438e03bad0076ced74297b4da2b0cc5e9102f
Konstantin Aksenov <kav.pbg@gmail.com>
2024-02-08 17:47:13 +0100

    showHidePassword implemented

---
+++ assets/worker/hideShowPassword.js
@@ -0,0 +1,17 @@
+/**
+ * To hide or show currentpassword in any fild.
+ * @param {each input from origin HTML-element} passwordId
+ * @param {each inputImage from origin HTML-element} imageId
+ */
+function changeToShowCurrentPassword(passwordId, imageId) {
+  let hideThePassword = document.getElementById(passwordId);
+  let hideThePasswordImage = document.getElementById(imageId);
+
+  if (hideThePassword.type == "password") {
+    hideThePassword.type = "text";
+    hideThePasswordImage.src = "/assets/img/login/lock.svg";
+  } else {
+    hideThePassword.type = "password";
+    hideThePasswordImage.src = "/assets/img/login/visibility_off.svg";
+  }
+}
